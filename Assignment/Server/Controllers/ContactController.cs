efc864a85607fee7344a2b3150122a2839ed1645
Applet pc <Applet pc@DESKTOP-IV6J8V1>
2023-05-16 12:43:27 -0700

    Add project files.

---
+++ Assignment/Server/Controllers/ContactController.cs
@@ -0,0 +1,21 @@
+﻿using Assignment.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContactController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Contact> Get()
+        {
+            List<Contact> contacts = new List<Contact>();
+            contacts.Add(new Contact { Id = 1, Name = "Salman", Email = "test1", Phone = 123 });
+            contacts.Add(new Contact { Id = 2, Name = "Ali", Email = "test2", Phone = 123 });
+            contacts.Add(new Contact { Id = 3, Name = "Abdullah", Email = "test3", Phone = 123 });
+            return contacts;    
+        }
+
+    }
+}
---
+++ Assignment/Shared/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Shared
+{
+    public class Contact
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Input Name Limit Exceeds", MinimumLength = 3)]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+        public string Message { get; set; }
+
+    }
+}
